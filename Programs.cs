7ad9aecbbe341707f8178b4150dc5863f5d88e86
CBell64 <72112627+CBell64@users.noreply.github.com>
2023-10-01 09:24:36 -0700

    Create Programs.cs

---
+++ Programs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//Joey Gallotta
+//CST-227 Milestone Project
+
+namespace CST_250_Milestone_Project
+{
+    internal class Program
+    {
+        //create instance of Board 
+        private static readonly Board Board = new Board(45);
+        private static BoardService _boardService;
+        private static readonly Game Game = new Game();
+        private static GameService _gameService;
+
+        public static void Main(string[] args)
+        {
+            //print empty game board
+            _boardService = new BoardService(Board);
+            _boardService.printBoard();
+            _boardService.countBombsNearCells();
+            Console.ReadLine();
+            _gameService = new GameService(Game,Board);
+            _gameService.GameLoop();
+        }
+    }
+}

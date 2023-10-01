12f79e4dbcfb016b901f61a863afd5ab3b3fc074
CBell64 <72112627+CBell64@users.noreply.github.com>
2023-10-01 09:20:11 -0700

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CST_250
+  _Milestone_Project
+{
+    public class Game
+    {
+        public bool gameOver { get; set; }
+
+    }
+}

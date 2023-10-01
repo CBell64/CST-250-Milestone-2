1ba069d33c13f16f0ba701a3ddb7fca3b303d7ab
CBell64 <72112627+CBell64@users.noreply.github.com>
2023-10-01 09:11:49 -0700

    Update CellService.cs

--- CellService.cs
+++ CellService.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CST_250_Milestone_Project
+{
+    class CellService
+    {
+        public Cell c;
+        public Board b;
+
+    }
+}
